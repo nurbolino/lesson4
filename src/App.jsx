3896c354195078a424d9eea450dfe635795605e3
Nurbol <nurbolgumagulov22212345@gmail.com>
2025-04-10 11:05:55 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import DataOfUsers from "./components/data/dataOfUsers";
+
+function App() {
+  
+
+
+
+
+  return (
+    <>
+       <h1>hallo boranes</h1>
+       <DataOfUsers />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/data/DataOfUsers.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const DataOfUsers = () => {
+  const users = [
+    {
+      name: "Alex",
+      userName: "111",
+      id: setTimeout(Date.now(), 1000),
+    },
+    {
+      name: "Bob",
+      userName: "222",
+      id: setTimeout(Date.now(), 2000),
+    },
+    {
+      name: "Gallaher",
+      userName: "333",
+      id: setTimeout(Date.now(), 3000),
+    },
+  ];
+
+  return (
+    <div>
+      <h1>data of users</h1>
+      {users.map((user) => (
+        <p>{` (Name ${user.name}) (UserName: ${user.userName}) (id: ${user.id})`}</p>
+      ))}
+    </div>
+  );
+};
+export default DataOfUsers;
+
+
+
+
+
